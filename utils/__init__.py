80daaae19592aeee5ba2437d352ad17c8ecc162d
Shuheng Liu <wish1104@outlook.com>
2018-11-01 02:50:16 +0800

    made the folder a python package

---
+++ utils/__init__.py
@@ -0,0 +1,17 @@
+from .args import get_environment_parameters
+from .trio import Trio
+from .image_utils import get_mean_std_stats, image2array, crop_folder, crop_image, resize_real
+from .init_op import get_init_op
+from .make_list import make_list
+
+__all__ = [
+    get_environment_parameters,
+    Trio,
+    get_mean_std_stats,
+    image2array,
+    crop_folder,
+    crop_image,
+    resize_real,
+    get_init_op,
+    make_list,
+]
