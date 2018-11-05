455d6015908c88fdf3ba7ecc2c4033f53d712b1f
Shuheng Liu <wish1104@outlook.com>
2018-11-05 23:17:42 +0800

    implemented base scorer and base binary scorer

---
+++ base/base_scorer.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+class BaseScorer(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @property
+    @abstractmethod
+    def score(self):
+        pass
+
+
+class BaseBinaryScorer:
+    def __init__(self, scorer1: BaseScorer, scorer2: BaseScorer):
+        self.scorer1 = scorer1
+        self.scorer2 = scorer2
+
+    def score1(self):
+        return self.scorer1.score
+
+    def score2(self):
+        return self.scorer2.score
