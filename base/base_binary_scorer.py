51e8b10016abae8802a72f8ca9348a870a8b26c8
Shuheng Liu <wish1104@outlook.com>
2018-11-05 23:47:44 +0800

    removed duplicate codes in base_scorer and base_duplicate_scorer

--- base/base_binary_scorer.py
+++ base/base_binary_scorer.py
@@ -1,19 +1,5 @@
 from abc import ABC, abstractmethod
-
-
-class BaseScorer(ABC):
-    @abstractmethod
-    def __init__(self):
-        pass
-
-    @property
-    @abstractmethod
-    def score(self):
-        """
-        returns the score of one GAN computed against ground truths
-        :rtype: float
-        """
-        pass
+from .base_scorer import BaseScorer
 
 
 class BaseBinaryScorer:
--- base/base_scorer.py
+++ base/base_scorer.py
@@ -14,21 +14,3 @@ class BaseScorer(ABC):
         :rtype: float
         """
         pass
-
-
-class BaseBinaryScorer:
-    def __init__(self, scorer1: BaseScorer, scorer2: BaseScorer):
-        self.scorer1 = scorer1
-        self.scorer2 = scorer2
-
-    @property
-    def score1(self):
-        return self.scorer1.score
-
-    @property
-    def score2(self):
-        return self.scorer2.score
-
-    @property
-    def scores(self):
-        return self.scorer1, self.scorer2
