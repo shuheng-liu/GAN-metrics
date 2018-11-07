22c8bba1688e8447839b2b2d23425c2b5773ccff
Shuheng Liu <wish1104@outlook.com>
2018-11-08 00:15:10 +0800

    uses custom Duo for real and fake latents

--- scorer/stats_scorer.py
+++ scorer/stats_scorer.py
@@ -1,4 +1,5 @@
 from base import BaseScorer
+from utils import Duo
 import numpy as np
 from PIL.Image import Image
 
@@ -6,8 +7,7 @@ from PIL.Image import Image
 class StatsScorer(BaseScorer):
     def __init__(self, images_real, images_fake):
         super(StatsScorer, self).__init__(images_real, images_fake)
-        self._latent1 = None
-        self._latent0 = None
+        self._latent_duo = Duo()
 
     @classmethod
     def _convert_to_array(cls, images):
@@ -29,13 +29,13 @@ class StatsScorer(BaseScorer):
     def _flatten(array):
         return np.reshape(array, [len(array), -1])
 
-    def _set_latents(self):
-        self._latent0 = self._flatten(self._convert_to_array(self._images0))
-        self._latent1 = self._flatten(self._convert_to_array(self._images1))
+    def _set_latent_duo(self):
+        latent0 = self._flatten(self._convert_to_array(self._images0))
+        latent1 = self._flatten(self._convert_to_array(self._images1))
+        self._latent_duo = Duo(real=latent1, fake=latent0)
 
     @property
-    def latents(self):
-        if self._latent0 is None or self._latent1 is None:
+    def latent_duo(self):
+        if self.latent_duo is None:
             self._set_latents()
-        return self._latent1, self._latent0
-
+        return self._latent_duo
